5487528971a0f4fcd816b977aff8cd1265a9b4cb
DekamaxX <dobosz.mateusz1994@gmail.com>
2024-02-25 16:46:55 +0100

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryElements = galleryItems
+  .map(picture => {
+    return `<a class="gallery__item" href="${picture.original}">
+       <img
+        class="gallery__image"
+         src="${picture.preview}"
+
+         alt="${picture.description}"
+       />
+     </a>
+   `;
+  })
+  .join('');
+
+galleryList.innerHTML = galleryElements;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'self',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
